380ce1da02e33bc0830b551c22ea9115d61d02e9
saitejababu <>
2020-09-02 16:14:36 +0530

    Updates

---
+++ precache-manifest.ec360b5bc8f95e93359387d0a04e7d50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b707698cbf1569dabc523b7675cd198",
+    "url": "/Tic-Tac-Toe_Game/index.html"
+  },
+  {
+    "revision": "142a588e77d61699b5b8",
+    "url": "/Tic-Tac-Toe_Game/static/css/main.9083aabd.chunk.css"
+  },
+  {
+    "revision": "4e753d5c3b58c1818a7c",
+    "url": "/Tic-Tac-Toe_Game/static/js/2.d8181919.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe_Game/static/js/2.d8181919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142a588e77d61699b5b8",
+    "url": "/Tic-Tac-Toe_Game/static/js/main.f5928906.chunk.js"
+  },
+  {
+    "revision": "d60456bef877af5bc88b",
+    "url": "/Tic-Tac-Toe_Game/static/js/runtime-main.60c12307.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe_Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
